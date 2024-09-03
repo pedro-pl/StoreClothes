96b19c50da956825067d4abc6c677c612b82ea5e
pedro <lucaspires2322@gmail.com>
2024-09-02 22:13:45 -0300

    fix: recriando o component section para correção de bug

--- src/pages/home/components/Section/index.tsx
+++ src/pages/home/components/Section/index.tsx
@@ -2,7 +2,7 @@
 import { shirt } from "../../../../mocks/shirts";
 import React from "react";
 import { Card } from "../card";
-import { Container, ContainerCard } from "./styles";
+import { Container, ContainerCard } from "./style";
 
 interface SectionProps {
     Title: string
