32470af165faabd644d6b2a297821b2ebbcc25b4
pedro <lucaspires2322@gmail.com>
2024-09-03 19:57:22 -0300

    feat: adicionando nova cor

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -3,6 +3,7 @@ export const defaultTheme = {
     'blue-100': '#caf0f8',
     'gray-900': '#292929',
     'green-200': '#52b788',
+    'green-300': '#40916c',
     'red': '#ef233c',
     'white': '#FFF',
     'off-white': '#fefae0',
