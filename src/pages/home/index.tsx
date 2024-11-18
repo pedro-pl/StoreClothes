1eeeb7531e45048174576b7fe63526d04471a5db
pedro <lucaspires2322@gmail.com>
2024-11-18 20:15:25 -0300

    fix: testando mudança de código

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,18 @@
-/* import { Footer } from "../../components/footer";
+import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 import { SearchBar } from "../../components/searchBar";
-import { Carousel } from "./components/sectionCarousel";
-import { Category } from "./components/sectionCategory";
-import { Container } from "./styles"; */
+import { Carousel } from "./components/sectionCarousel/sectionCarousel";
+import { Category } from "./components/sectionCategory/sectionCategory";
+import { Container } from "./styles";
 
 export function Home(){
     return(
-        //<Container>
-        //    <Header/>
-        //    <SearchBar/>
-        //    <Carousel Title="Lançamentos"/>
-        //    <Category Title="Categorias"/>
-        //    <Footer/>
-        //</Container>
-        <h1>teste</h1>
+        <Container>
+            <Header/>
+            <SearchBar/>
+            <Carousel Title="Lançamentos"/>
+            <Category Title="Categorias"/>
+            <Footer/>
+        </Container>
     )
 }
