a3b27a703518479a9ed9108fafd7e35404e8edea
pedro <lucaspires2322@gmail.com>
2024-11-18 19:38:48 -0300

    feat: adicionando novas cores

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,6 +1,9 @@
 export const defaultTheme = {
     'black': '#000',
     'blue-100': '#caf0f8',
+    'gray-100': '#e9ecef',
+    'gray-300': '#ced4da',
+    'gray-500': '#adb5bd',
     'gray-900': '#292929',
     'green-200': '#52b788',
     'green-300': '#40916c',
