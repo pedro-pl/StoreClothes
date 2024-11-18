2298b46c557332386bd95459348d7fbd4d66883d
pedro <lucaspires2322@gmail.com>
2024-11-18 19:34:41 -0300

    feat: criando mock para ícones

---
+++ src/mocks/categorys.tsx
@@ -0,0 +1,36 @@
+import { IoShirtOutline } from "react-icons/io5";
+import { PiBaseballCap, PiPants, PiSneaker } from "react-icons/pi";
+import { IoGlassesOutline } from "react-icons/io5";
+
+export const categorys = [
+    {
+        id: 0,
+        name: "Camisetas",
+        icon: <IoShirtOutline/>,
+        link: "#"
+    },
+    {
+        id: 1,
+        name: "Calças",
+        icon: <PiPants/>,
+        link: "#"
+    },
+    {
+        id: 2,
+        name: "Bonés",
+        icon: <PiBaseballCap/>,
+        link: "#"
+    },
+    {
+        id: 3,
+        name: "Calçados",
+        icon: <PiSneaker/>,
+        link: "#"
+    },
+    {
+        id: 4,
+        name: "Acessórios",
+        icon: <IoGlassesOutline/>,
+        link: "#"
+    }
+]
